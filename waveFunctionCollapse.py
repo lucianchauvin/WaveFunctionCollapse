c552400d0d546c414b634391d3d80df5a8879d9d
Lucian Chauvin <lucianchauvin@gmail.com>
2022-04-21 10:07:30 -0500

    fixed bug

--- waveFunctionCollapse.py
+++ waveFunctionCollapse.py
@@ -2,16 +2,12 @@ import random
 
 wfc = {
     "A": {
-        "img": "blank.png",
-        "adjacency": [["A", "B"], ["A"], ["A", "B"], ["A"]],
+        "img": "",
+        "adjacency": [["A"], ["A","B"], ["A"], ["A","B"]],
     },
     "B": {
-        "img": "bar.png",
-        "adjacency": [["A", "B"], ["B", "C"], ["A", "B"], ["B"]],
-    },
-    "C": {
-        "img": "corrner.png",
-        "adjacency": [["A", "B"], ["A"], [], ["B"]],
+        "img": "",
+        "adjacency": [["B"], ["A", "B"], ["B"], ["A", "B"]],
     },
 }
 
@@ -23,21 +19,21 @@ def collapse(tiles, x, y):
     possibleStates = wfc.copy()
 
     idkHowToExplainThis = [(2, (0, 1)), (3, (1, 0)), (0, (0, -1)), (1,(-1, 0))]
-
     
     for direction in idkHowToExplainThis:
-        if y < len(tiles)-1 and x < len(tiles[0])-1:
+        try:
             tile = tiles[y+direction[1][1]][x+direction[1][0]]
             if tile is not None:
                 for state in wfc:
-                    print(state, wfc[tile]["adjacency"][direction[0]])
-                    if state not in wfc[tile]["adjacency"][direction[0]]  and state in possibleStates:
+                    if state not in wfc[tile]["adjacency"][direction[0]] and state in possibleStates:
                         possibleStates.pop(state)
+        except:
+            pass
 
     if len(possibleStates) == 0:
         print("NO POSSIBLE STATES")
         return None
-    print(possibleStates, x, y)
+    # print(possibleStates, x, y)
     return random.choice(list(possibleStates))
 
 
@@ -54,6 +50,3 @@ for(x, y) in [(x, y) for x in range(10) for y in range(10)]:
     tiles[y][x] = collapse(tiles, x, y)
 
 printTiles(tiles)
-
-
-# print(collapse(tiles, 1, 0))
