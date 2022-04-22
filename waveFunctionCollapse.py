1cd9d60af3791fd8705d3048cc4b09af410e39f2
Lucian Chauvin <lucianchauvin@gmail.com>
2022-04-21 21:34:43 -0500

    final

--- waveFunctionCollapse.py
+++ waveFunctionCollapse.py
@@ -1,35 +1,50 @@
-import imghdr
 import random
-
-from regex import R
-from sympy import to_cnf
 
 wfc = {
     "A": {
-        "img": "╬",
-        "adjacency": [["A","C" ], ["A","B" ], ["A", "C"], ["A","B"]],
+        "img": "┼",
+        "adjacency": [["A", "C", "E", "F", ], ["A", "B", "E", ], ["A", "C", "E", "F"], ["A", "B", "F"]],
     },
     "B": {
-        "img": "═",
-        "adjacency": [["B","D"], ["A", "B", "E"], ["B","D"], ["A", "B"]],
+        "img": "─",
+        "adjacency": [["B", "D"], ["A", "B", "E", ], ["B", "D", ], ["A", "B", "F"]],
     },
-    "C":{
-        "img": "║",
-        "adjacency": [["C","A"], ["C","D"], ["C","A"], ["C","D"]],
+    "C": {
+        "img": "│",
+        "adjacency": [["C", "A", "F", "E", ], ["C", "D", "F"], ["C", "A", "E", "F"], ["C", "D", "E"]],
     },
-    "D":{
+    "D": {
         "img": " ",
-        "adjacency": [["B","D"], ["C","D"], ["B","D"], ["C","D"]],
+        "adjacency": [["B", "D"], ["C", "D", "F"], ["B", "D", ], ["C", "D", ]],
     },
-    "E":{
-        "img": "╣",
-        "adjacency": [["C","A"], ["C","D"], ["C","A"], ["A","B"]],
+    "E": {
+        "img": "┤",
+        "adjacency": [["C", "A", "F", ], ["C", "D", "F"], ["C", "A", "F"], ["A", "B", "F"]],
     },
-    "F":{
-        "img" : "╠",
-        "adjacency": [["C","E","A"], ["B","A","E"], ["a"], ["F","G"]],
-    }
+    "F": {
+        "img": "├",
+        "adjacency": [["C", "E", "A", ], ["B", "A", "E", ], ["A", "C", "E"], ["E", "C", "D", ]],
+    },
 }
+
+# wfc = {
+#     "A": {
+#         "img": "┌",
+#         "adjacency": [["C", "D"], ["B", "D"], ["C", "D"], ["B", "D"]],
+#     },
+#     "B": {
+#         "img": "┐",
+#         "adjacency": [["C", "D"], ["A", "C"], ["C", "D"], ["A", "C"]],
+#     },
+#     "C": {
+#         "img": "└",
+#         "adjacency": [["A", "B"], ["B", "D"], ["A", "B"], ["B", "D"]],
+#     },
+#     "D": {
+#         "img": "┘",
+#         "adjacency": [["A", "B"], ["C", "A"], ["A", "B"], ["C", "A"]],
+#     },
+# }
 
 
 def collapse(tiles, x, y):
@@ -38,8 +53,9 @@ def collapse(tiles, x, y):
 
     possibleStates = wfc.copy()
 
-    idkHowToExplainThis = [(2, (0, 1)), (3, (1, 0)), (0, (0, -1)), (1,(-1, 0))]
-    
+    idkHowToExplainThis = [(2, (0, 1)), (3, (1, 0)),
+                           (0, (0, -1)), (1, (-1, 0))]
+
     for direction in idkHowToExplainThis:
         try:
             tile = tiles[y+direction[1][1]][x+direction[1][0]]
@@ -51,7 +67,7 @@ def collapse(tiles, x, y):
             pass
 
     if len(possibleStates) == 0:
-        print("NO POSSIBLE STATES")
+        print("----NO POSSIBLE STATES----")
         return None
     # print(possibleStates, x, y)
     return random.choice(list(possibleStates))
@@ -62,14 +78,15 @@ def printTiles(tiles):
         print(row)
 
 
-tiles = [[None for x in range(125)] for x in range(20)]
+tiles = [[None for x in range(167)] for x in range(20)]
 
-tiles[0][0] = random.choice(list(wfc))  
+tiles[0][0] = "A"
 
 for r in range(20):
-    for c in range(125):
+    for c in range(167):
         tiles[r][c] = collapse(tiles, c, r)
 
-tilesWithImg = "\n".join(["".join([wfc[tile]["img"] for tile in row]) for row in tiles])
+tilesWithImg = "\n".join(["".join([wfc[tile]["img"]
+                         for tile in row]) for row in tiles])
 
 print(tilesWithImg)
