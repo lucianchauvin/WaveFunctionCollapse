2fa4542a1c7c004b6e7467304407e868d5b1c660
Lucian Chauvin <lucianchauvin@gmail.com>
2022-04-27 09:39:09 -0500

    fixed croping bug with odd NxN matrix

--- waveFunctionCollapse.py
+++ waveFunctionCollapse.py
@@ -6,6 +6,7 @@ import json
 
 # generates the wave function for a given image with an n x m states
 def generateWaveFunction(img, n, m):
+
     img = Image.open(img)
 
     width, height = img.size
@@ -15,11 +16,13 @@ def generateWaveFunction(img, n, m):
     states = {}
 
     c = 0
-    for (j, i) in [(i, j) for i in range(n) for j in range(m)]:
+    for (i, j) in [(i, j) for i in range(n) for j in range(m)]:
         top = j*stateWidth
         left = i*stateHeight
-        bottom = (j+1)*stateWidth
-        right = (i+1)*stateHeight
+        bottom = top + stateWidth
+        right = left + stateWidth
+
+        print(left, top, right, bottom)
 
         s = img.crop((left, top, right, bottom))
         s.save("stateImages/" + ascii_lowercase[c] + ".png")
@@ -34,10 +37,23 @@ def generateWaveFunction(img, n, m):
 
         c += 1
 
+        for x in range(3):
+            s = s.rotate(90)
+            pixelsNew = np.array(s)
+            currentAdj = [states[state]["adj"] for state in states]
+            adj = [pixelsNew[0].tolist(), [pixelsNew[y][len(pixelsNew[0])-1].tolist()
+                                           for y in range(len(pixelsNew))], pixelsNew[len(pixelsNew)-1].tolist(), [pixelsNew[y][0].tolist() for y in range(len(pixelsNew))]]
+            if adj not in currentAdj:
+                s.save("stateImages/" + ascii_lowercase[c] + ".png")
+                states.update(
+                    {ascii_lowercase[c]: {"img": "stateImages/" + ascii_lowercase[c] + ".png", "adj": adj}})
+
+                c += 1
+
     with open("states.json", "w") as f:
         json.dump(states, f)
 
     return states
 
 
-generateWaveFunction("img.png", 3, 3)
+generateWaveFunction("img.png", 4, 1)
