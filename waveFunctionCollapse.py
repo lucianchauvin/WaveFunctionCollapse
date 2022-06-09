ef65c26da354eb8020820bf264799b8a77de0e43
Lucian Chauvin <lucianchauvin@gmail.com>
2022-06-08 19:08:29 -0500

    done2

--- waveFunctionCollapse.py
+++ waveFunctionCollapse.py
@@ -63,22 +63,33 @@ def generateWave(states, w, h):
 
 #directions = [(2, (0, -1)), (3, (1, 0)),(0, (0, 1)), (1, (-1, 0))]
 
-def propogate(wave, states, x, y):  # broken https://www.youtube.com/watch?v=2SuvO4Gi7uY&t=766s
-    stack = []
-    stack.append((x, y))
+def propogate(wave, states, x, y):  # updates the wave based off of a collapse event
+    # initalizes the stack of updated cells with the first element being the collapsed cell
+    stack = [(x, y)]
+    # the list used to iterate through the nesw directions. the first value of the tuple is the index value of the opposite cell adjecencies. So if we want the adjacencies of the cell to the north while our current cell is below we want the 3 list in the adjacencies list which corosponds to a 2 index value
     directions = [(2, (0, -1)), (3, (1, 0)), (0, (0, 1)), (1, (-1, 0))]
-    while len(stack) > 0:
+    while len(stack) > 0:  # while the stack has updated cells in it
+        # get the last value in the stack to update other cells
         current = stack.pop(-1)
+        # if the cell is collapsed, so if there is only 1 possible state
         if len(wave[current[1]][current[0]]) == 1:
+            # get all adjacencies of collapsed cell and the state of that collapsed cell
             currentAdj = states[wave[current[1]][current[0]][0]]['adj']
+            # iterate through the all cells around whilist iterating through the corosponding adjaciencies of each cell
             for direction in enumerate(directions):
+                # gets the corosponding adjacencies of current cell. so if we are on the cell to the north we need the north adjaciencies to compare with
                 currentRelAdj = currentAdj[direction[0]]
                 other = (current[0] + direction[1][1][0],
-                         current[1] + direction[1][1][1])
+                         current[1] + direction[1][1][1])  # gets the x and y cords of other cell
+                # checks if other is valid cell
                 if other[0] < len(wave[0]) and other[0] >= 0 and other[1] < len(wave) and other[1] >= 0:
+                    # get possibilities of other cell
                     otherPos = wave[other[1]][other[0]].copy()
+                    # for all possible states in other cell
                     for pos in wave[other[1]][other[0]]:
+                        # gets the relative adjacencies of the other cell. so if we are on the cell to the north we need the south adjaciencies to compare with
                         posRelAdj = states[pos]['adj'][direction[1][0]]
+                        # if the other relative adjacency is not the same as our current one remove it from possible states
                         if currentRelAdj != posRelAdj:
                             if len(otherPos) > 1:
                                 otherPos.remove(pos)
@@ -117,7 +128,7 @@ def getLeastEntropy(wave, states):
     return random.choice(minList)
 
 
-def generateImage(wave, states, w, h, pixelSize):
+def generateImage(wave, states, w, h, pixelSize, name):
     img = Image.new("RGB", (w*pixelSize, h*pixelSize))
     for y in range(h):
         for x in range(w):
@@ -125,7 +136,7 @@ def generateImage(wave, states, w, h, pixelSize):
                 state = wave[y][x][0]
                 img.paste(Image.open(states[state]["img"]),
                           (x*pixelSize, y*pixelSize))
-    img.save("wave.png")
+    img.save(name)
     img.show()
 
 
@@ -135,20 +146,21 @@ def printWave(wave):
     print("\n")
 
 
-s = generateStates("mesh.png", 2, 1)
+for x in range(20):
+    s = generateStates("mesh.png", 2, 1)
 
-width = 80
-height = 45
+    width = 10
+    height = 10
 
-wave = generateWave(s, width, height)
+    wave = generateWave(s, width, height)
 
-i = 0
-while not is_collapsed(wave):
-    i += 1
-    min = getLeastEntropy(wave, s)
-    wave = collapse(wave, s, min[0], min[1])
-    wave = propogate(wave, s, min[0], min[1])
-    print(i*100/(width*height))
+    i = 0
+    while not is_collapsed(wave):
+        i += 1
+        min = getLeastEntropy(wave, s)
+        wave = collapse(wave, s, min[0], min[1])
+        wave = propogate(wave, s, min[0], min[1])
+        print(i*100/(width*height))
 
-printWave(wave)
-generateImage(wave, s, width, height, 24)
+    printWave(wave)
+    generateImage(wave, s, width, height, 24, "wave" + str(x) + ".png")
